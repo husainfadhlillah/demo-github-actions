0b16512d1ca526624becd1790dcd081d84016a62
MUHAMMAD HUSAIN FADHLILLAH <151012069+husainfadhlillah@users.noreply.github.com>
2025-09-03 21:20:36 +0700

    update model.py

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 # model.py
 
 import pandas as pd
-impor os  # Modul ini sengaja diimpor untuk menunjukkan error
+import os  # sudah diperbaiki
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
